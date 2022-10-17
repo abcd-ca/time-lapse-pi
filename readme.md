d56e9136eb0b0cf8c4829f6b44f1d2df87ff0803
abcd-ca <andrew@abcd.ca>
2022-10-17 12:45:18 -0700

    Now it takes n pictures on the rpi with raspistill

--- readme.md
+++ readme.md
@@ -3,6 +3,8 @@
 There was a [camera crate](https://github.com/pedrosland/rascam) for the legacy Raspberry Pi OS that kind of worked but I found it easy enough to just call the external `raspistill` command. 
  A newer version of the distro doesn't come with `raspicam` set of commands, it comes with the open source, [`libcamera`](https://www.raspberrypi.com/documentation/accessories/camera.html#libcamera-and-the-legacy-raspicam-camera-stack) library which has a C++ API but no-one has created a Rust crate to interface with it yet. Maybe I could create that crate? In the meantime, I can work on a general interval based application that takes pictures every n seconds, saves images and maybe later stitches them together (or have another binary in this same package to stitch them)  
 
+TODO update OS and try the newer CLI command
+
 ## linking to libcamera library
 
 Should need C++ `.h` files but not source files to link. [This crate](https://crates.io/crates/bindgen) might help automatically expose the API. That's how the rascam crate uses the raspicam library (which [binds to the mmal-sys library using bindgen](https://github.com/pedrosland/mmal-sys/blob/master/Cargo.toml))
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::error::Error;
 use std::process::Command;
 use std::fs;
+use std::env;
+use std::error::Error;
+// use std::fmt;
+use std::time::Duration;
+use tokio::{task, time}; // 1.3.0
 
-fn main() -> Result<(), Box<dyn Error>> {
-    fs::create_dir_all("./output")?;
-
+async fn capture_image(n: u32) -> Result<(), Box<dyn Error  + Send + Sync>>  {
     // command exists on legacy versions of raspian only
     let output = Command::new("raspistill")
         .arg("-o")
-        .arg("output/cam.jpg")
+      //println!("{:0width$}", x, width = width);
+        .arg(format!("output/image{:0width$}.jpg", n + 1, width = 4))
         .output()?;
-    // raspistill -t 21600000 -tl 300000 -o image%06d.jpg
+    // let output = Command::new("ls")
+    //     .arg("-l")
+    //     .output()?;
 
     if !output.status.success() {
         // error_chain::bail!("Command executed with failing error code");
@@ -38,6 +43,59 @@ fn main() -> Result<(), Box<dyn Error>> {
         // })
         .take(5)
         .for_each(|x| println!("{:?}", x));
-
     Ok(())
+}
+
+fn help() {
+    println!("usage: time-lapse-pi 25")
+}
+
+//-> Result<(), Box<dyn Error>>
+#[tokio::main]
+async fn main()  {
+    let forever = task::spawn(async {
+        let mut x: u32 = 0;
+
+        let args: Vec<String> = env::args().collect();
+        if args.len() != 2 {
+            help();
+            std::process::exit(1)
+        }
+        let num = &args[1];
+
+        // parse the number
+        let total_screenshots: u32 = match num.parse() {
+            Ok(n) => n,
+            Err(_) => {
+                eprintln!("error: second argument not an integer");
+                help();
+                std::process::exit(1)
+            }
+        };
+
+        println!("Will create {} screenshots...", total_screenshots);
+
+        fs::create_dir_all("./output").expect("Should create the output directory");
+
+        loop {
+            match capture_image(x).await {
+                Ok(()) => {
+                    if x < total_screenshots - 1 {
+                        x += 1;
+                        println!("Captured {} of {}", x, total_screenshots);
+                        time::sleep(Duration::from_millis(60 * 1000)).await;
+                    } else {
+                        println!("Finished. {} screenshots captured.", total_screenshots);
+                        std::process::exit(1)
+                    }
+                }
+                Err(_) => {
+                    eprintln!("Encountered error, exiting");
+                    std::process::exit(1)
+                }
+            }
+        }
+    });
+
+    forever.await.expect("TODO panic message");
 }
