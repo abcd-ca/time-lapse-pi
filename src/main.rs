30f75f25ab583f464b4c824be57e5930a39e0ba1
abcd-ca <andrew@abcd.ca>
2022-10-22 21:32:49 -0700

    move capture_image into camera module

--- src/main.rs
+++ src/main.rs
@@ -2,67 +2,12 @@ use std::env;
 use std::error::Error;
 use std::fs;
 use std::process::Command;
-// use std::fmt;
 use std::time::Duration;
+use time_lapse_pi::camera;
 use time_lapse_pi::gpio::{button, led};
 use tokio::{task, time}; // 1.3.0
 
 extern crate args;
-
-// TODO args https://crates.io/crates/args
-
-// ref: `libcamera-jpeg -h` and https://www.raspberrypi.com/documentation/accessories/camera.html#libcamera-jpeg
-async fn capture_image(n: u32) -> Result<(), Box<dyn Error + Send + Sync>> {
-    // taking stills in a rust loop instead of using the built-in libcamera timelapse so that we can provide GPIO feedback and do disk storage checks
-    let output = Command::new("libcamera-jpeg")
-        .arg("-o")
-        .arg(format!(
-            "output/image{:0pad_width$}.jpg",
-            n + 1,
-            pad_width = 4
-        ))
-        .arg("--immediate")
-        .arg("--width")
-        .arg("1280")
-        .arg("--height")
-        .arg("720")
-        .arg("--quality")
-        .arg("85")
-        .output()?;
-    // TODO add a delay argument so I can start it at 10pm and it will start recording when I know sunrise is, like at 6am the next day
-    // TODO estimate disk space needed and warn
-    // TODO check disk space between pictures and quit before running out.
-
-    if !output.status.success() {
-        // error_chain::bail!("Command executed with failing error code");
-        println!("Something went wrong, exiting");
-
-        String::from_utf8(output.stderr)?
-            .lines()
-            // .filter_map(|line| pattern.captures(line))
-            // .map(|cap| Commit {
-            //     hash: cap[1].to_string(),
-            //     message: cap[2].trim().to_string(),
-            // })
-            .take(5)
-            .for_each(|x| eprintln!("{:?}", x));
-
-        std::process::exit(1)
-    }
-
-    String::from_utf8(output.stdout)?
-        .lines()
-        // .filter_map(|line| pattern.captures(line))
-        // .map(|cap| Commit {
-        //     hash: cap[1].to_string(),
-        //     message: cap[2].trim().to_string(),
-        // })
-        .take(5)
-        .for_each(|x| println!("{:?}", x));
-
-    Ok(())
-}
-
 fn help() {
     println!("usage: time-lapse-pi 25")
 }
@@ -97,7 +42,7 @@ async fn main() {
 
         loop {
             led::blink();
-            match capture_image(x).await {
+            match camera::capture_image(x).await {
                 Ok(()) => {
                     if x < total_screenshots - 1 {
                         x += 1;
